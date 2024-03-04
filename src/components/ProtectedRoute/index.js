e569ccda4f88628e124ba03ef1d8e2912155994e
venkatvsm <venkatmani368@gmail.com>
2024-03-04 17:16:18 +0000

    Nxt Trendz Protected Route

---
+++ src/components/ProtectedRoute/index.js
@@ -0,0 +1,11 @@
+// Write your JS code here
+import Cookies from 'js-cookie'
+import {Route, Redirect} from 'react-router-dom'
+
+const ProtectedRoute = props => {
+  if (Cookies.get('jwt_token') === undefined) {
+    return <Redirect to="/login" />
+  }
+  return <Route {...props} />
+}
+export default ProtectedRoute
